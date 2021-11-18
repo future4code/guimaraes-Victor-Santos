ec1c455d0abb02bb119d2a098ba19ecfe7162470
victor-hugo <vicorbhlanna24@yahoo.com>
2021-11-17 22:38:27 -0300

    exercicio-string-arrays

---
+++ modulo1/string-arrays/template_javascript/index.js
@@ -0,0 +1,55 @@
+//Exercicio de escrita
+
+//Exercicio 1.
+
+//const qualSeuNome = prompt("Qual é o seu nome?")
+//const qualSeuEmail = prompt("Qual é o seu Email ?")
+
+//console.log(`O e-mail ${qualSeuEmail} foi cadastrado com sucesso. Seja bem vindo(a) ${qualSeuNome}.`)
+
+//---------------------------------//------------------------//
+
+//Exercicio 2
+
+//const comidasPreferidas = ["Feijoada", "tropeiro", "parmegiana", "batata-frita", "churrasco"]
+//console.log("Estas são minhas comidas preferidas", comidaPreferida)
+
+//Desafio
+//console.log(comidasPreferidas[0])
+//console.log(comidasPreferidas[1])
+//console.log(comidasPreferidas[2])
+//console.log(comidasPreferidas[3])
+//console.log(comidasPreferidas[4])
+
+//comidasPreferidas[1] = prompt("Qual é a sua comida predileta ?")
+//console.log(comidasPreferidas)
+
+//--------------------------------//------------------------//
+
+//Exercicio 3
+
+//const listaDeTarefas = []
+//listaDeTarefas[0] = prompt("Diga uma tarefa")
+//listaDeTarefas[1] = prompt("Diga uma tarefa")
+//listaDeTarefas[2] = prompt("Diga uma tarefa")
+//console.log(listaDeTarefas)
+
+//const tarefas = prompt (` digite o índice de uma tarefa que já realizou, 0 para ${listaDeTarefas[0]}, 1 para ${listaDeTarefas[1]} 2 para ${listaDeTarefas[2]}`)
+//listaDeTarefas.pop(tarefas)
+//console.log(listaDeTarefas)
+
+
+//-------------------------------//----------------------//
+
+//Exercicio de interpretação de codigo
+
+//const frase = prompt("Digite uma frase")
+
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//Resposta = SUBI NUM ONIBUS EM MIRROCUS
+
+
+//------------------------------//------------------------//
+
+//Como está em cima, vou deixar os desafios para os estudos do final de semana.
