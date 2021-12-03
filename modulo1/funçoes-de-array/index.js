87c16d77bc8b7a99012808b5f77676a50db15291
victor-hugo <vicorbhlanna24@yahoo.com>
2021-12-02 22:23:42 -0300

    Exercicio-Funçoes-de-array

---
+++ modulo1/funçoes-de-array/index.js
@@ -0,0 +1,70 @@
+//Exercicio de escrita de código
+
+/* const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" }
+  ]
+  
+  const novoArrayA = usuarios.map((item, index, array) => {
+     console.log(item, index, array)
+  }) */
+
+  // Vai ser impresso;
+  //apelido: "Mandi"
+/* nome: "Amanda Rangel" */
+
+//apelido: "Laura"
+//nome: "Laís Petra"
+
+//apelido: "Chijo"
+//nome: "Letícia Chijo"
+
+//0: {nome: 'Amanda Rangel', apelido: 'Mandi'}
+/* 1: {nome: 'Laís Petra', apelido: 'Laura'}
+2: {nome: 'Letícia Chijo', apelido: 'Chijo'}
+length: 3 */
+
+
+/* const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" },
+  ]
+  
+  const novoArrayB = usuarios.map((item, index, array) => {
+     return item.nome
+  })
+  
+  console.log(novoArrayB) */
+
+
+
+
+
+//Exercicio leitura de código 2
+
+
+/* const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" },
+  ]
+  
+  const novoArrayB = usuarios.map((item, index, array) => {
+     return item.nome
+  })
+  
+  console.log(novoArrayB) */
+
+
+
+//Resposta
+ /*  0: "Amanda Rangel"
+1: "Laís Petra"
+2: "Letícia Chijo" */
+
+
+
+
+
