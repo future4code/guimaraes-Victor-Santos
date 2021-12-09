72d502cc08a0d0fc8299b90f0f53473955e4a368
victor-hugo <vicorbhlanna24@yahoo.com>
2021-12-09 20:49:51 -0300

    Exercicio-8

--- modulo1/SEMANA 3/Revisão JS/lista-revisao-js/exercicios.js
+++ modulo1/SEMANA 3/Revisão JS/lista-revisao-js/exercicios.js
@@ -118,7 +118,18 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
+     
    
+        let numerosPares = []
+    
+        for(let i = 0; numerosPares.length < n; i++){
+        if (i % 2 === 0){
+            numerosPares.push(i)
+        }
+    
+        }
+        return numerosPares
+    
     
 }
 
