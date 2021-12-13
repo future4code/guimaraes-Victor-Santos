7a06c0625fee6f896f3eb9542cd6c9866b8d03b1
victor-hugo <vicorbhlanna24@yahoo.com>
2021-12-12 22:23:51 -0300

    Projeto-Blackjack

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -12,9 +12,26 @@
  */
 
     
-    console.log(`Bem vindo ao jogo Blackjack !`)
-   
-    if (confirm("Quer iniciar uma nova rodada ?")){
-    console.log("Iniciar uma nova rodada")
-
-    } else{ console.log("O jogo acabou")}
+    if (confirm('Quer iniciar uma nova rodada?')) {
+    
+        console.log('Boas vindas ao jogo de Blackjack!');
+     
+        const primeiraCartaDoUsuario = comprarCarta();
+        const segundaCartaDoUsuario = comprarCarta();
+        const pontosUsuario = primeiraCartaDoUsuario.valor + segundaCartaDoUsuario.valor;
+        console.log(`Usuário - cartas ${primeiraCartaDoUsuario.texto} ${segundaCartaDoUsuario.texto} - pontuação: ${pontosUsuario}`);
+     
+        const primeiraCartaDoComputador = comprarCarta();
+        const segundaCartaDoComputador = comprarCarta();
+        const pontosComputador = primeiraCartaDoComputador.valor + segundaCartaDoComputador.valor;
+        console.log(`Computador - cartas  ${primeiraCartaDoComputador.texto} ${segundaCartaDoComputador.texto} - pontuação: ${pontosComputador}`);
+        if (pontosUsuario > pontosComputador) {
+            console.log(`O usuário ganhou!`);
+        } else if (pontosUsuario < pontosComputador) {
+            console.log(`O computador ganhou!`);
+        } else {
+            console.log(`Empate!`);
+        }
+     } else {
+        console.log('O jogo acabou');
+     }
